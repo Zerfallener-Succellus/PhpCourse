65616f6083788694fe2c562815f72b072b5f2939
Lucas Godoy <pedrolucf@gmail.com>
2022-10-24 20:46:00 -0300

    "primeira parte do modulo 2 (completar amanhã)"

---
+++ tipos/desafio_string.php
@@ -0,0 +1,11 @@
+<div class="titulo">Desafio String</div>
+
+<?php
+
+//Enunciado:
+//Avaliando os métodos da documentação da string
+//qual o método que a posição do texto "abc"
+//na string "!Abcabcabc" retorne 1?
+
+echo strpos('!Abcabcabc', 'abc') . '<br>';
+echo strpos('!Abcabcabc', 'Abc') . '<br>';
---
+++ tipos/int.php
@@ -0,0 +1,16 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php 
+
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 011, '<br>'; //Base octal
+echo 0b11000110001, '<br>'; //Base Binária
+echo 0x117acf0, '<br>'; //Base Hexadecimal
+
